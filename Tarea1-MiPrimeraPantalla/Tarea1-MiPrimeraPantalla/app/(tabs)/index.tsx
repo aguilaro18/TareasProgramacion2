002bec7013dafe075605c62de2dd9a5b2d96253e
aguilaro18 <aylinjuditha@gmail.com>
2026-08-06 00:04:47 -0600

    Creacion del archivo, creacion de los componentes y configuracion de la fotografia

---
+++ Tarea1-MiPrimeraPantalla/Tarea1-MiPrimeraPantalla/app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { View, Text, Image, Pressable, StyleSheet, Alert } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.contenedor}>
+      <Image
+        source={require("../../assets/images/foto.jpeg")}
+        style={styles.foto}
+      />
+      <Text style={styles.nombre}>Aylin Aguilar</Text>
+      <Text style={styles.carrera}>Ingeniería en Sistemas</Text>
+      <Text style={styles.carnet}>Carné: 2026-20-XXXXX</Text>
+      <Pressable style={styles.boton}>
+        <Text style={styles.botonTexto}>Ver proyectos</Text>
+      </Pressable>
+    </View>
+  );
+}
+
