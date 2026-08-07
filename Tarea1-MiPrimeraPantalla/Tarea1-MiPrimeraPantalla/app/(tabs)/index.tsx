b46330a8dc778ff21cfbde7bbe91345f0b4c2e8f
aguilaro18 <aylinjuditha@gmail.com>
2026-08-06 20:58:12 -0600

    Se utilizó el método StyleSheet.create con las propiedasdes tipografía, espaciado y bordes para dar formato y centrar la carta de presentacion

--- Tarea1-MiPrimeraPantalla/Tarea1-MiPrimeraPantalla/app/(tabs)/index.tsx
+++ Tarea1-MiPrimeraPantalla/Tarea1-MiPrimeraPantalla/app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ export default function App() {
       />
       <Text style={styles.nombre}>Aylin Aguilar</Text>
       <Text style={styles.carrera}>Ingeniería en Sistemas</Text>
-      <Text style={styles.carnet}>Carné: 2026-20-XXXXX</Text>
+      <Text style={styles.carnet}>Carné: 0907-25-6317</Text>
       <Pressable style={styles.boton}>
         <Text style={styles.botonTexto}>Ver proyectos</Text>
       </Pressable>
@@ -18,5 +18,11 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  contenedor: { flex: 1, alignItems: "center", justifyContent: "center", padding: 16 },
+  foto: { width: 120, height: 120, borderRadius: 60, marginBottom: 16 },
+  nombre: { fontSize: 24, fontWeight: "bold", color:  "#f566c5" },
+  carrera: { fontSize: 16, color: "#bfc37a", marginTop: 4 },
+  carnet: { fontSize: 14, color: "#888", marginTop: 4 },
+  boton: { marginTop: 20, backgroundColor: "#bfc37a", padding: 12, borderRadius: 8 },
+  botonTexto: { color: "white", fontSize: 16 }
 });
