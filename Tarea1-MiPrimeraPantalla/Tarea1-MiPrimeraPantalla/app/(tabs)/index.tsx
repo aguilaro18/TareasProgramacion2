463ee61677a851a551da9c977c641c1be6db5468
aguilaro18 <aylinjuditha@gmail.com>
2026-08-07 21:51:47 -0600

    Intento de agregar funcionalidad al boton

--- Tarea1-MiPrimeraPantalla/Tarea1-MiPrimeraPantalla/app/(tabs)/index.tsx
+++ Tarea1-MiPrimeraPantalla/Tarea1-MiPrimeraPantalla/app/(tabs)/index.tsx
@@ -10,7 +10,10 @@ export default function App() {
       <Text style={styles.nombre}>Aylin Aguilar</Text>
       <Text style={styles.carrera}>Ingeniería en Sistemas</Text>
       <Text style={styles.carnet}>Carné: 0907-25-6317</Text>
-      <Pressable style={styles.boton}>
+      <Pressable
+        style={styles.boton}
+        onPress={() => Alert.alert("Seré ingeniera")}
+      >
         <Text style={styles.botonTexto}>Ver proyectos</Text>
       </Pressable>
     </View>
@@ -20,9 +23,12 @@ export default function App() {
 const styles = StyleSheet.create({
   contenedor: { flex: 1, alignItems: "center", justifyContent: "center", padding: 16 },
   foto: { width: 120, height: 120, borderRadius: 60, marginBottom: 16 },
-  nombre: { fontSize: 24, fontWeight: "bold", color:  "#f566c5" },
+  nombre: { fontSize: 24, fontWeight: "bold", color: "#f566c5" },
   carrera: { fontSize: 16, color: "#bfc37a", marginTop: 4 },
   carnet: { fontSize: 14, color: "#888", marginTop: 4 },
   boton: { marginTop: 20, backgroundColor: "#bfc37a", padding: 12, borderRadius: 8 },
   botonTexto: { color: "white", fontSize: 16 }
 });
+
+
+
