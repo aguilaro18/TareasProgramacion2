c5fb6771b4baef986f47bb8fed2982b6b398e348
aguilaro18 <aylinjuditha@gmail.com>
2026-08-12 19:12:50 -0600

    Definicin de funciones y condicionales

--- Tarea2-ListasEfectosEventos/Tarea2-ListasEfectosEventos/app/(tabs)/index.tsx
+++ Tarea2-ListasEfectosEventos/Tarea2-ListasEfectosEventos/app/(tabs)/index.tsx
@@ -7,3 +7,40 @@ export default function App() {
 }
 
 // Guardar y cocultar el imput 
+const guardarTarea = () => {
+  if (textoNuevaTarea.trim() === '') return;
+  setTareas([...tareas, textoNuevaTarea]);
+  setTextoNuevaTarea('');
+  setMostrarInput(false);
+}; 
+
+//Confimar y eliminar tarea compatible en wed o telefono
+const confirmarEliminar = (index: number) =>{
+  const mensaje = "Estas seguro que deseas eliminar la tarea?";
+
+if (Platform.OS === 'web'){ 
+  const acepto = window.confirm(mensaje); 
+  if (acepto){
+    const nuevasTareas = tareas.filter ((_, i) => i  !== index);
+    setTareas(nuevasTareas);
+  }
+}else{
+  Alert.alert(
+    "Eliminar tarea", 
+  mensaje,
+  [
+    {text: "Cancelar", style: "cancel"},
+    {
+      text: "Si",
+      onPress: () => {
+        const nuevasTareas = tareas.filter ((_, i) => i  !== index);
+        setTareas(nuevasTareas);
+    
+      }
+    }
+
+  ]
+
+  );
+}
+};
